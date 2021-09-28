b6762d96c6eddc2941df431bcf1862d0121a9ab8
Ziyadhs <ziyadhs2001@gmail.com>
2021-09-28 17:28:14 +0300

    save lab 13 backend by adding create and delete functions

--- modules/bookHandler.js
+++ modules/bookHandler.js
@@ -2,7 +2,7 @@
 
 const { bookModel } = require("./book");
 
-
+//function to get all data inside db
 let getBookHandler = (req, res) => {
     let email1 = req.query.email;
     bookModel.find({email:email1}).then(data => {
@@ -13,7 +13,34 @@ let getBookHandler = (req, res) => {
     });
 }
 
+//function to create a new book
+let createBookHandler = (req, res) => {
+
+    let bookData = req.body;
+    let newBook = new bookModel(bookData);
+    newBook.save();
+    bookModel.find().then(data => {
+        res.status(200).json(data);
+    }).catch((error) => {
+        res.status(500).send('error there is no recived data');
+    });
+}
+
+//function to delete a specific data from db
+let deleteBookHandler = (req, res) => {
+
+    let id = req.params.id;
+    bookModel.findByIdAndDelete(id).then(() => {
+        bookModel.find().then(data => {
+            res.status(200).json(data);
+        })
+    }).catch((error) => {
+        res.status(500).send('error there is no file');
+    });
+}
 
 module.exports = {
     getBookHandler,
+    createBookHandler,
+    deleteBookHandler,
 }
